526aae8c1d3826876ef8b5ed2fd47bf1631ee223
Pawan C Paleja <ppaleja@its-gl1w-mac05.ucsd.edu>
2019-11-22 12:41:12 -0800

    Started project, around ~17 minutes into walkthrough

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -19,7 +19,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func onTap(_ sender: Any) {
+        print("Hello")
+        view.endEditing(true)
+    }
+    
 
 }
 
